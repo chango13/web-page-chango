3a83cc9e855fcf69c1c1887a3517b968014928da
chango13 <arielguillaume@gmail.com>
2020-08-12 10:26:37 -0300

    primer commit

---
+++ src/static/scripts/script.js
@@ -0,0 +1,11 @@
+    function change(elemento){
+        elemento.classList.toggle('bar');        
+        var show = document.querySelector('.items');
+        show.classList.toggle('mostrar')
+        console.log('mostrando')
+    }
+    
+
+    
+
+    
